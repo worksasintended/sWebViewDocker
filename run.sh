012f495662d645c6c6274e90a5310a95623f31dd
root <root@minicluster.minicluster>
2018-03-02 12:15:13 +0100

    changed run from interactive to --rm

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ SLURMCONFIG="/etc/slurm-llnl/slurm.conf"
 MUNGEKEY="/etc/munge/munge.key"
 sWebView="`pwd`/sWebView"
 docker run \
-	-it \
+	--rm \
 	-v $SLURMCONFIG:/etc/slurm-llnl/slurm.conf \
 	-v $MUNGEKEY:/etc/munge/munge.key \
 	-v $sWebView:/sWebView \
 	-p 8080:8080 \
-	local/swebview bash
+	local/swebview 
