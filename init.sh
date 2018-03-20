926eabf4897fd9ab3efff9e666a7dd08e33f606d
Marc Marschall <mb144122@minicluster.minicluster>
2018-03-20 12:49:25 +0100

    minor changes

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 cd /sWebView/src
 cmake .
 make -j16
-sudo -u munge munged &
+sudo -u munge munged  --num-threads=10 &
 ./run.sh
--- run_it.sh
+++ run_it.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 SLURMCONFIG="/etc/slurm-llnl/slurm.conf"
-MUNGEKEY="$PWD/munge.key"
+MUNGEKEY="/etc/munge/munge.key"
 sWebView="`pwd`/sWebView"
 docker run \
-	--rm \
+	-it \
 	--name "slurmwebview" \
 	-v $SLURMCONFIG:/etc/slurm-llnl/slurm.conf \
 	-v $MUNGEKEY:/etc/munge/munge.key \
 	-v $sWebView:/sWebView \
 	-p 8080:8080 \
-	local/swebview
+	local/swebview bash
